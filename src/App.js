d623250aa4e2230e5c822f3f50548564ab0ce007
mahehehes <mahestifajar@gmail.com>
2024-02-27 15:50:28 +0800

    book-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
+  const [books, setBooks] = useState([]);
+
+  function handleAddBook(book) {
+    setBooks([book, ...books]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddBook} />
+      <Bookshelf books={books} />
+      <Stats books={books} />
+      <Footer />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1> Book Lover's 📚 </h1>;
+}
+
+function Form({ onAddItems }) {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [status, setStatus] = useState("unread");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!title || !author) return;
+    const newBook = { title, author, status, id: Date.now() };
+    onAddItems(newBook);
+    setTitle("");
+    setAuthor("");
+    setStatus("unread");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="add-form">
+      <h2>Tambahkan Buku Baru 📖</h2>
+      <input
+        type="text"
+        placeholder="Judul"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Penulis"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+      <select value={status} onChange={(e) => setStatus(e.target.value)}>
+        <option value="unread">Belum Dibaca</option>
+        <option value="reading">Sedang Dibaca</option>
+        <option value="read">Sudah Dibaca</option>
+      </select>
+      <button type="submit">Tambahkan Buku</button>
+    </form>
+  );
+}
+
+function Bookshelf({ books }) {
+  return (
+    <div className="list">
+      <h2>List Buku Mu :</h2>
+      <ul>
+        {books.map((book) => (
+          <Book key={book.id} book={book} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Book({ book }) {
+  return (
+    <li className={`book ${book.status}`}>
+      <span>{book.title} oleh {book.author}</span>
+    </li>
+  );
+}
+
+function Stats({ books }) {
+  return (
+    <footer className="stats">
+      <em>
+        📚 Kamu memiliki {books.length} buku di rak, dan sudah membaca {books.filter(book => book.status === "read").length} buku.
+      </em>
+    </footer>
+  );
+}
+
+function Footer() {
+  return null;
+}
